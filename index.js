b18bb3966d15bac9031a6bbd81ca21e438ab2975
Jason DeLine <jcdeline1982@gmail.com>
2023-05-01 21:57:45 -0500

    defining SVGCreator class

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const fs = require('fs')
 const inquirer = require("inquirer");
-const {Circle, Square, Triangle} = require("./lib/shapes");
+const {CircleShape, SquareShape, TriangleShape} = require("./lib/shapes");
 
+class SVGCreator {
+    constructor() {
+        this.textElement = ''
+        this.shapeElement = ''
+    }
+    render() {
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeEl}${this.textEl}</svg>`
+    }
+    setTextEl(text, color) {
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeEl(shape) {
+        this.shapeEl = shape.render()
+    }
+}
